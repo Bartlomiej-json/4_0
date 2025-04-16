77cd0b0d5ab2d4df3bab946fad43eb95139a21f2
Bartlomiej-json <bartlomiej.serafin.gnom@gmail.com>
2025-04-16 13:51:35 +0000

    4_1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,20 +8,31 @@ Kod bazowy programu Commit4_0:
 */
 
 import java.io.IOException;
+import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
     try {
       Service s = new Service();
-      s.addStudent(new Student("Krzysztof", 20));
-      s.addStudent(new Student("Janusz", 40));
+      Scanner scanner = new Scanner(System.in);
+
+      System.out.print("Podaj imię studenta: ");
+      String imie = scanner.nextLine();
+
+      System.out.print("Podaj wiek studenta: ");
+      int wiek = Integer.parseInt(scanner.nextLine());
+
+      s.addStudent(new Student(imie, wiek));
+
+      System.out.println("Student dodany!");
+      scanner.close();
 
       var students = s.getStudents();
-      for(Student current : students) {
+      for (Student current : students) {
         System.out.println(current.ToString());
       }
     } catch (IOException e) {
-
+      System.out.println("Błąd operacji na pliku: " + e.getMessage());
     }
   }
 }
